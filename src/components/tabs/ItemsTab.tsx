4b0c8d0979741ac310520797c86e93b7d5062ac2
Christian Dionisio <christiandionisio9432@gmail.com>
2023-04-16 11:54:22 -0500

    scrollable principal content added

--- src/components/tabs/ItemsTab.tsx
+++ src/components/tabs/ItemsTab.tsx
@@ -58,6 +58,34 @@ export const ItemsTab = () => {
             </div>
         </div>
 
+        <div className="items-list-section">
+            <h2>Fruit and vegetables</h2>
+            <div className="grid-items-cards">
+                <ItemCard name={'Avocado'} />
+                <ItemCard name={'Banana'} />
+                <ItemCard name={'Bunch of carrots 5pcs'} />
+                <ItemCard name={'Chicken 1kg'} />
+                <ItemCard name={'Pre-cooked corn 450g'} />
+                <ItemCard name={'Mandarin Nadorcott'} />
+                <ItemCard name={'Piele De Sapo Melon'} />
+                <ItemCard name={'Watermelon'} />
+            </div>
+        </div>
+
+        <div className="items-list-section">
+            <h2>Fruit and vegetables</h2>
+            <div className="grid-items-cards">
+                <ItemCard name={'Avocado'} />
+                <ItemCard name={'Banana'} />
+                <ItemCard name={'Bunch of carrots 5pcs'} />
+                <ItemCard name={'Chicken 1kg'} />
+                <ItemCard name={'Pre-cooked corn 450g'} />
+                <ItemCard name={'Mandarin Nadorcott'} />
+                <ItemCard name={'Piele De Sapo Melon'} />
+                <ItemCard name={'Watermelon'} />
+            </div>
+        </div>
+
     </div>
   )
 }
