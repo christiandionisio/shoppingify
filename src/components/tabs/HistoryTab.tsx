4da2901262dbb190913f814f6701097acfa4a31f
Christian Dionisio <christiandionisio9432@gmail.com>
2023-04-19 20:53:42 -0500

    history tab html added

---
+++ src/components/tabs/HistoryTab.tsx
@@ -0,0 +1,33 @@
+
+
+export const HistoryTab = () => {
+  return (
+    <div className="history-tab">
+        <h1>Shopping history</h1>
+        <div className="monthly-history-list">
+            <h3>August 2020</h3>
+            <div className="history-list">
+                <div className="history-item">
+                    <div className="left">
+                        <h6>Grocery List</h6>
+                    </div>
+
+                    <div className="right">
+                        <span className="material-symbols-outlined">
+                            calendar_month
+                        </span>
+                        <p>Mon 27.8.2020</p>
+                        <div className="badge">
+                            completed
+                        </div>
+                        <span className="material-symbols-outlined">
+                            arrow_forward_ios
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/pages/TabsPage.tsx
+++ src/pages/TabsPage.tsx
@@ -1,10 +1,12 @@
-import { ItemsTab } from '../components/tabs/ItemsTab'
+// import { ItemsTab } from '../components/tabs/ItemsTab'
+import { HistoryTab } from '../components/tabs/HistoryTab'
 import '../styles/pages/TabsPage.css'
 
 export const TabsPage = () => {
   return (
     <div className="tabs-page">
-      <ItemsTab />
+      {/* <ItemsTab /> */}
+      <HistoryTab />
     </div>
   )
 }
