9409961e9db43093ec6bbdb7ab9b0416dde166a0
Christian Dionisio <christiandionisio9432@gmail.com>
2023-04-22 17:33:21 -0500

    StatisticsProgressElementList css added

--- src/components/ui/StatisticsProgressElementList.tsx
+++ src/components/ui/StatisticsProgressElementList.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { StatisticsProgressElement } from "./StatisticsProgressElement";
+import '../../styles/components/ui/StatisticsProgressElementList.css';
 
 interface Props {
     elementType: string;
